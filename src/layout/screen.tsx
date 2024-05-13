3ab090dee1b410ac868a0f8b005e0206f21d17b4
Hamilton Lumati <hamiltonlumati@gmail.com>
2024-05-13 23:20:16 +0100

    update

--- src/layout/screen.tsx
+++ src/layout/screen.tsx
@@ -1,6 +1,22 @@
 function Screen(){
     return(
-        <div className="w-80 h-auto bg-black p-2 grid justify-center">
+        <div className="w-80 h-auto bg-yellow p-2 grid justify-center">
+            <div className="grid grid-cols-3 gap-4">
+                <div>
+                    <p>Break Lenght</p>
+                    <div>
+                        25
+                    </div>
+                </div>
+                <div>
+                    <p>Session Lenght</p>
+                    <div>
+                        25
+                    </div>
+
+                </div>
+
+            </div>
             <div className="grid grid-cols-1 gap-0">
                 <div className="bg-white p-1">
                     25:00
