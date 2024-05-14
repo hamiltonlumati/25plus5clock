67b74aff6b20569857cf10eb9e6fe07f3333b97d
Hamilton Lumati <hamiltonlumati@gmail.com>
2024-05-14 14:32:19 +0100

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,12 @@ export default {
     "./src/layout/screen.tsx"
   ],
   theme: {
+    colors: {
+      'darkGreen': '#003E1F',
+      'lightGreen': 'D5F2E3',
+      'orange': 'orange',
+      'dark': '01110A'
+    },
     extend: {},
   },
   plugins: [],
