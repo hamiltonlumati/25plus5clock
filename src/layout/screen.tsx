571730727ff95e779a8a545e3b4282432042f6ad
Hamilton Lumati <hamiltonlumati@gmail.com>
2024-05-15 00:32:58 +0100

    update

--- src/layout/screen.tsx
+++ src/layout/screen.tsx
@@ -1,6 +1,6 @@
 function Screen(){
     return(
-        <div className="h-auto bg-darkGreen p-2 grid justify-center rounded">
+        <div className="h-auto bg-darkGreen pb-10 grid justify-center rounded">
             <div className="grid grid-cols-2 gap-20 justify-center w-full px-20 py-10">
                 <div className="w-auto">
                     <p className="text-center text-lightGreen mb-2 text-xl font-medium">Break Lenght</p>
@@ -27,12 +27,23 @@ function Screen(){
                     </div>
                 </div>
             </div>
-            <div className="grid grid-cols-1 gap-0">
-                <div className="bg-white p-1">
-                    25:00
-                </div>
-                <div className="bg-">
-                    <span>Vida</span>
+            <div className="grid justify-center rounded bg-dark text-lightGreen w-auto mx-52 py-2">
+                <p className="bg-white p-1 text-center text-lg">
+                    Session
+                </p>
+                <p className="text-center text-7xl">
+                    60:00
+                </p>
+                <div className="flex flex-row gap-4 align-middle px-14 mt-2">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-play-circle hover:text-orange" viewBox="0 0 16 16">
+                        <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
+                        <path d="M6.271 5.055a.5.5 0 0 1 .52.038l3.5 2.5a.5.5 0 0 1 0 .814l-3.5 2.5A.5.5 0 0 1 6 10.5v-5a.5.5 0 0 1 .271-.445"/>
+                    </svg>
+                    
+                    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-arrow-repeat hover:text-orange" viewBox="0 0 16 16">
+                        <path d="M11.534 7h3.932a.25.25 0 0 1 .192.41l-1.966 2.36a.25.25 0 0 1-.384 0l-1.966-2.36a.25.25 0 0 1 .192-.41m-11 2h3.932a.25.25 0 0 0 .192-.41L2.692 6.23a.25.25 0 0 0-.384 0L.342 8.59A.25.25 0 0 0 .534 9"/>
+                        <path fill-rule="evenodd" d="M8 3c-1.552 0-2.94.707-3.857 1.818a.5.5 0 1 1-.771-.636A6.002 6.002 0 0 1 13.917 7H12.9A5 5 0 0 0 8 3M3.1 9a5.002 5.002 0 0 0 8.757 2.182.5.5 0 1 1 .771.636A6.002 6.002 0 0 1 2.083 9z"/>
+                    </svg>
                 </div>
             </div>
         </div>
