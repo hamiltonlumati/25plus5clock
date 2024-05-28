20a71e75c896fb0892b05227af0b872a891a27b2
Hamilton Lumati <hamiltonlumati@gmail.com>
2024-05-28 18:33:51 +0100

    update

--- src/layout/screen.tsx
+++ src/layout/screen.tsx
@@ -1,7 +1,7 @@
 import { useReducer } from "react";
 import moment from "moment";
 import { RootState } from "../app/store";
-import { UseSelector, useDispatch } from "react-redux";
+import { UseSelector, useDispatch, useSelector } from "react-redux";
 import { 
     sessionDecrement, 
     sessionIncrement, 
@@ -152,7 +152,11 @@ function Screen(){
     const [state, dispatch] = useReducer(reducer, initialState);
  */
 
-    const time = 
+    const sessionLenght = useSelector((state: RootState) => state.timer.sessionLenght);
+    const breakLenght = useSelector((state: RootState) => state.timer.breakLenght);
+    const startButtonClass = useSelector((state: RootState) => state.timer.startButtonClass);
+    const stopButtonClass = useSelector((state: RootState) => state.timer.stopButtonClass);
+    const dispatch = useDispatch();
     return(
         <div className="h-auto bg-darkGreen pb-10 grid justify-center rounded">
             <div className="grid grid-cols-2 gap-20 justify-center w-full px-20 py-10">
@@ -162,7 +166,7 @@ function Screen(){
                         <svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" fill="currentColor" className="bi bi-arrow-up-circle basis-1/3 h-auto hover:text-dark" viewBox="0 0 16 16" onClick={() => {dispatch({type: 'sessionIncrement'})}}>
                             <path className="align-middle" fillRule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-7.5 3.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707z"/>
                         </svg>
-                        <span className="basis-1/3 text-center text-5xl">{Math.floor(state.sessionLenght)}</span>
+                        <span className="basis-1/3 text-center text-5xl">{Math.floor(sessionLenght)}</span>
                         <svg  xmlns="http://www.w3.org/2000/svg" width="64" height="64" fill="currentColor" className="bi bi-arrow-down-circle basis-1/3 h-auto hover:text-dark" viewBox="0 0 16 16" onClick={() => {dispatch({type: 'sessionDecrement'})}}>
                             <path fillRule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.5 4.5a.5.5 0 0 0-1 0v5.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293z"/>
                         </svg>
@@ -174,7 +178,7 @@ function Screen(){
                         <svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" fill="currentColor" className="bi bi-arrow-up-circle basis-1/3 h-auto hover:text-dark" viewBox="0 0 16 16" onClick={() => {dispatch({type: 'breakIncrement'})}}>
                             <path className="align-middle" fillRule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-7.5 3.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707z"/>
                         </svg>
-                        <span className="basis-1/3 text-center text-5xl">{state.breakLenght}</span>
+                        <span className="basis-1/3 text-center text-5xl">{breakLenght}</span>
                         <svg  xmlns="http://www.w3.org/2000/svg" width="64" height="64" fill="currentColor" className="bi bi-arrow-down-circle basis-1/3 h-auto hover:text-dark" viewBox="0 0 16 16" onClick={() => {dispatch({type: 'breakDecrement'})}}>
                             <path fillRule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.5 4.5a.5.5 0 0 0-1 0v5.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293z"/>
                         </svg>
@@ -189,12 +193,12 @@ function Screen(){
                     60:00
                 </p>
                 <div className="flex flex-row gap-4 align-middle px-14 mt-2">
-                    <svg id="startButton" xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className={state.startButtonClass} viewBox="0 0 16 16" onClick={() => {dispatch({type: 'timerStart'})}}>
+                    <svg id="startButton" xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className={startButtonClass} viewBox="0 0 16 16" onClick={() => {dispatch({type: 'timerStart'})}}>
                         <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
                         <path d="M6.271 5.055a.5.5 0 0 1 .52.038l3.5 2.5a.5.5 0 0 1 0 .814l-3.5 2.5A.5.5 0 0 1 6 10.5v-5a.5.5 0 0 1 .271-.445"/>
                     </svg>
 
-                    <svg id="stopButton" xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className={state.stopButtonClass} viewBox="0 0 16 16">
+                    <svg id="stopButton" xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className={stopButtonClass} viewBox="0 0 16 16">
                         <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
                         <path d="M5 6.25a1.25 1.25 0 1 1 2.5 0v3.5a1.25 1.25 0 1 1-2.5 0zm3.5 0a1.25 1.25 0 1 1 2.5 0v3.5a1.25 1.25 0 1 1-2.5 0z"/>
                     </svg>
